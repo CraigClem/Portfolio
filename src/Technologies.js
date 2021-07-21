047fe02f78760000e2c6b1cc5e713f9e96a1fd21
Craigc <craigclement@hotmail.com>
2021-07-21 17:54:49 +0100

    changes

--- src/Technologies.js
+++ src/Technologies.js
@@ -8,60 +8,79 @@ function Technologies() {
       <div className="tech-container">
         <div className="tech-icon">
           <i className="devicon-javascript-plain colored"></i>
+        
         </div>
         <div className="tech-icon">
           <i className="devicon-react-original colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-css3-plain colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-django-line-wordmark colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-github-original-wordmark colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-html5-plain colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-jest-plain colored"></i>
+         
         </div>
         <div className="tech-icon">
           <i className="devicon-mongodb-plain-wordmark colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-nodejs-plain colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-npm-original-wordmark colored"></i>
+         
         </div>
         <div className="tech-icon">
           <i className="devicon-postgresql-plain-wordmark colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-python-plain-wordmark colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-sass-original colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-slack-plain-wordmark"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-trello-plain-wordmark colored"></i>
+         
         </div>
         <div className="tech-icon">
           <i className="devicon-vscode-plain colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-babel-plain colored"></i>
+          
         </div>
         <div className="tech-icon">
           <i className="devicon-express-original-wordmark colored"></i>
+         
         </div>
         <div className="tech-icon">
           <i className="devicon-heroku-line colored"></i>
+          
         </div>
       </div>
     </div>

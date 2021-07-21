b7b2c194410c1ff8ff1b7bc5a7aa85ceb223283b
Craigc <craigclement@hotmail.com>
2021-07-21 16:25:41 +0100

    changes

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ function About() {
         <img src="https://i.imgur.com/BxlHG90.png"/>
       </div>
       <div className="hello">
-        <h1>Hello, Im <span className="name">Craig,</span><br/>A <span className="name">Junior </span> FullStack <span className="name">Developer</span> based in <span className="name">London</span></h1>
+        <h1>Hello, Im <span className="name">Craig,</span><br/>A <span className="name">Junior </span> FullStack <span className="name">Developer</span><br/> based in <span className="name">London</span></h1>
 
 
       </div>

0ec821c788bc8053a0b8fcb35d926f77ae2543f3
Craigc <craigclement@hotmail.com>
2021-07-27 10:55:38 +0100

    technolgy text onlcick

--- src/Technologies.js
+++ src/Technologies.js
@@ -13,118 +13,118 @@ function Technologies() {
     <div className="tech" id="tech"> 
       <div className="tech-container">
         <div className="tech-icon" value="JavaScript">
-          <i className="devicon-javascript-plain colored" value="JavaScript" onClick={handleClick}></i>
+          <i className="devicon-javascript-plain colored" onClick={handleClick}></i>
           {isShown && 
         <p>JAVASCRIPT</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-react-original colored"></i>
+          <i className="devicon-react-original colored" onClick={handleClick}></i>
           {isShown && 
         <p>REACT</p>
           }
         </div>
         
         <div className="tech-icon">
-          <i className="devicon-css3-plain colored"></i>
+          <i className="devicon-css3-plain colored" onClick={handleClick}></i>
         
           {isShown && 
         <p>CSS3</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-django-line-wordmark colored"></i> 
+          <i className="devicon-django-line-wordmark colored" onClick={handleClick}></i> 
        
           {isShown && 
         <p>DJANGO</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-github-original-wordmark colored"></i>
+          <i className="devicon-github-original-wordmark colored" onClick={handleClick}></i>
           {isShown && 
         <p>GITHUB</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-html5-plain colored"></i>
+          <i className="devicon-html5-plain colored" onClick={handleClick}></i>
           {isShown && 
         <p>HTML5</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-jest-plain colored"></i>   
+          <i className="devicon-jest-plain colored" onClick={handleClick}></i>   
           {isShown && 
         <p>JEST</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-mongodb-plain-wordmark colored"></i>
+          <i className="devicon-mongodb-plain-wordmark colored" onClick={handleClick}></i>
           {isShown && 
         <p>MONGO DB</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-nodejs-plain colored"></i> 
+          <i className="devicon-nodejs-plain colored" onClick={handleClick}></i> 
           {isShown && 
         <p>NODE.JS</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-npm-original-wordmark colored"></i>
+          <i className="devicon-npm-original-wordmark colored" onClick={handleClick}></i>
           {isShown && 
         <p>NPM</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-postgresql-plain-wordmark colored"></i>
+          <i className="devicon-postgresql-plain-wordmark colored" onClick={handleClick}></i>
           {isShown && 
         <p>POSTGRESQL</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-python-plain-wordmark colored"></i>
+          <i className="devicon-python-plain-wordmark colored" onClick={handleClick}></i>
           {isShown && 
         <p>PYTHON</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-sass-original colored"></i>
+          <i className="devicon-sass-original colored" onClick={handleClick}></i>
           {isShown && 
         <p>SASS</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-slack-plain-wordmark"></i>  
+          <i className="devicon-slack-plain-wordmark" onClick={handleClick}></i>  
           {isShown && 
         <p>SLACK</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-trello-plain-wordmark colored"></i>
+          <i className="devicon-trello-plain-wordmark colored" onClick={handleClick}></i>
           {isShown && 
         <p>TRELLO</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-vscode-plain colored"></i> 
+          <i className="devicon-vscode-plain colored" onClick={handleClick}></i> 
           {isShown && 
         <p>VS CODE</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-babel-plain colored"></i>
+          <i className="devicon-babel-plain colored" onClick={handleClick}></i>
           {isShown && 
         <p>BABLE</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-express-original-wordmark colored"></i>
+          <i className="devicon-express-original-wordmark colored" onClick={handleClick}></i>
           {isShown && 
         <p>EXPRESS</p>
           }
         </div>
         <div className="tech-icon">
-          <i className="devicon-heroku-line colored"></i>
+          <i className="devicon-heroku-line colored" onClick={handleClick}></i>
           {isShown && 
         <p>HEROKU</p>
           }
